4a98d4df36c0ef3b75a9bfd2c01db87a92d2211c
mariyamnavila <bibimariyamnavila@gmail.com>
2025-07-13 10:05:08 +0600

    added transactions

--- js/login.js
+++ js/login.js
@@ -34,7 +34,7 @@ document.getElementById('button-login')
         console.log(phoneNumber,pinNumber);
         if (phoneNumber === '5' && pinNumber === '1234') {
             console.log('you are logged in');
-            window.location.href = '/payoo-mfs/home.html';
+            window.location.href = '/payoo-mfs/home2.html';
         } 
         else {
             alert('wrong phone number or Pin');
